513539cd5bec1922e54d1118aa5ed49de1be469c
Andrea Duran <andrea.duran@mail.escuelaing.edu.co>
2022-05-13 13:30:26 -0500

    soniditoBeio

--- src/presentation/DadosGUI.java
+++ src/presentation/DadosGUI.java
@@ -3,10 +3,14 @@ package presentation;
 import domain.Dado;
 import domain.Parchis;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class DadosGUI extends JPanel {
     private int valor1 = 1;
@@ -20,6 +24,7 @@ public class DadosGUI extends JPanel {
                 parchis.tirarDado();
                 valor1 = parchis.getValor1();
                 valor2 = parchis.getValor2();
+                ReproducirSonido();
                 repaint();
 
             }else{
@@ -64,6 +69,22 @@ public class DadosGUI extends JPanel {
         setOpaque(false);
         addMouseListener(clic);
         setVisible(true);
+    }
+
+    public void ReproducirSonido(){
+        try {
+            //URL url = getClass().getResource("/audios/dados.wav");
+            //File audio = new File("wav2.wav");
+            String nombreSonido = "src/audios/dados.wav";
+
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+
+        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            System.out.println("Error al reproducir el sonido. " + ex);
+        }
     }
     public void paint(Graphics g){
         Image img1 = new ImageIcon(getClass().getResource("/imagenes/Dado"+valor1+".png")).getImage();
